9b1c3041544196d172a4248831b8a974906e61e1
C. Tyler Hart <zeaga15@gmail.com>
2018-06-29 12:14:42 -0700

    Apparently the entity doesn't exist on clients yet, gotta network the shit

--- lua/autorun/joinleavesounds.lua
+++ lua/autorun/joinleavesounds.lua
@@ -1,8 +1,9 @@
 if SERVER then
 	util.AddNetworkString( "ZeagaPlayerJoin" )
 	hook.Add( "PlayerInitialSpawn", "ZeagaPlayerJoin", function( ply )
-		net.Start( "ZeagaPlayerJoin" )
-			net.WriteEntity( ply )
+		net.Start( "ZeagaPlayerJoin" ) -- We send the team/name and not the entity because it may not yet exist on the clients.
+			net.WriteUInt( ply:Team( ), 4 )
+			net.WriteString( ply:Name( ) )
 		net.Broadcast( )
 	end )
 	util.AddNetworkString( "ZeagaPlayerLeave" )
@@ -20,8 +21,7 @@ if CLIENT then
 		"garrysmod/save_load4.wav",
 	}
 	net.Receive( "ZeagaPlayerJoin", function( length )
-		local ply = net.ReadEntity( )
-		chat.AddText( team.GetColor( ply:Team( ) ), ply:Name( ), Color( 255, 255, 255 ), " has spawned in the server!" )
+		chat.AddText( team.GetColor( net.ReadUInt( 4 ) ),net.ReadString( ), Color( 255, 255, 255 ), " has spawned in the server!" )
 		surface.PlaySound( table.Random( joinSounds ) )
 	end )
 	local leaveSounds = {
