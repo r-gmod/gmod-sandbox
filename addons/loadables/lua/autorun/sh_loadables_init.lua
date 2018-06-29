c874a1e803fb2cd33ec4247edc8dcb8ab556313d
John Chandara <mrpotatofactory@gmail.com>
2018-06-29 16:15:50 -0500

    Added new loadables addon + Optimized join/leave loadable

---
+++ addons/loadables/lua/autorun/sh_loadables_init.lua
@@ -0,0 +1,92 @@
+/**
+	Loadables
+	Created by Potatofactory <mrpotatofactory@gmail.com>
+
+	For public use
+**/
+
+local funcFormat    = string.format
+local include       = include
+local AddCSLuaFile  = SERVER and AddCSLuaFile or nil
+
+local LOADABLE_SERVER = 1
+local LOADABLE_CLIENT = 2
+local LOADABLE_SHARED = 3
+
+local tblRealmActions = SERVER and
+{
+    [LOADABLE_SERVER] = function (strScript)
+        include (funcFormat (
+            "loadables/server/%s",
+            strScript
+        ))
+    end
+    [LOADABLE_CLIENT] = function (strScript)
+        AddCSLuaFile (funcFormat (
+            "loadables/client/%s",
+            strScript
+        ))
+    end
+    [LOADABLE_SHARED] = function (strScript)
+        local strFullPath = funcFormat (
+            "loadables/server/%s",
+            strScript
+        )
+
+        include         (strFullPath)
+        AddCSLuaFile    (strFullPath)
+    end
+}
+
+or 
+
+{
+    [LOADABLE_SERVER] = function (strScript)
+        ErrorNoHalt (funcFormat (
+            "[loadables] Client has server-side script: %s",
+            strScript
+        ))
+    end
+    [LOADABLE_CLIENT] = function (strScript)
+        include (funcFormat (
+            "loadables/client/%s",
+            strScript
+        ))
+    end
+    [LOADABLE_SHARED] = function (strScript)
+        include         (strFullPath)
+    end 
+}
+
+if SERVER then
+    AddCSLuaFile ()
+end
+
+_G.loadables = {}
+
+function loadables:RefreshIndex ()
+    loadables.index = {
+        {file.Find ("loadables/server/*",  "LUA")},
+        {file.Find ("loadables/client/*",  "LUA")}
+        {file.Find ("loadables/*",         "LUA")},
+    }
+
+    return loadables.index
+end
+
+function loadables:ReloadAll ()
+    local tblIndex = loadables:RefreshIndex ()
+
+    for iteration = 1, #tblIndex do
+        local tblScripts = tblIndex [iteration][1]
+        for iteration_script = 1, #tblScripts do
+            tblRealmActions [iteration] (tblScripts [iteration_script])
+        end
+    end
+end
+
+////////////////////////////////////////
+//   Do not touch the line below.     //
+////////////////////////////////////////
+        loadables:ReloadAll ()
+////////////////////////////////////////
---
+++ addons/loadables/lua/loadables/joinleavesounds.lua
@@ -0,0 +1,61 @@
+if SERVER then
+	util.AddNetworkString( "ZeagaPlayerAttendance" )
+	hook.Add( "PlayerInitialSpawn", "ZeagaPlayerJoin", function( ply )
+		net.Start( "ZeagaPlayerAttendance" ) -- We send the team/name and not the entity because it may not yet exist on the clients.
+			net.WriteBool( true )
+			net.WriteUInt( ply:Team( ), 4 )
+			net.WriteString( ply:Name( ) )
+		net.Broadcast( )
+	end )
+	util.AddNetworkString( "ZeagaPlayerLeave" )
+	hook.Add( "PlayerDisconnected", "ZeagaPlayerLeave", function( ply )
+		net.Start( "ZeagaPlayerAttendance" )
+			net.WriteBool( false )
+			net.WriteUInt( ply:Team( ), 4 )
+			net.WriteString( ply:Name( ) )
+		net.Broadcast( )
+	end )
+end
+
+if CLIENT then
+	local TYPE_SPAWN 		= 1
+	local TYPE_DISCONNECT	= 2
+
+	local tblAttendenceSounds = {
+		[TYPE_SPAWN] = {
+			"garrysmod/save_load1.wav",
+			"garrysmod/save_load4.wav",
+		}
+		[TYPE_DISCONNECT] = {
+			"garrysmod/save_load2.wav",
+			"garrysmod/save_load3.wav",
+		}
+	}
+	net.Receive( "ZeagaPlayerAttendance", function( length )
+		local bArriving = net.ReadBool 	 ()
+		local iTeamId	= net.ReadUInt	 (4)
+		local strName	= net.ReadString ()
+
+		local iEventType = bArriving and TYPE_SPAWN or TYPE_DISCONNECT
+
+		if bArriving then
+			chat.AddText (
+				team.GetColor( iTeamId ),
+				strName,
+				Color( 255, 255, 255 ),
+				" has spawned in the server!"
+			)
+		else
+			chat.AddText (
+				team.GetColor( iTeamId ),
+				strName,
+				Color( 255, 255, 255 ),
+				" has left the server!"
+			)
+		end
+
+		local tblSounds = tblAttendenceSounds [iEventType]
+		surface.PlaySound (tblSounds [math.random (1, #tblSounds)])
+
+	end )
+end
