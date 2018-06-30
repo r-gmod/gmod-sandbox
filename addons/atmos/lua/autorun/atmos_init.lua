ddd437f21c3c90ff604999a0efac56da29b5ff1f
C. Tyler Hart <zeaga15@gmail.com>
2018-06-30 15:22:21 -0700

    Added Atmos

---
+++ addons/atmos/lua/autorun/atmos_init.lua
@@ -0,0 +1,16 @@
+AddCSLuaFile();
+AddCSLuaFile( "atmos/shared.lua" );
+AddCSLuaFile( "atmos/cl_init.lua" );
+
+include( "atmos/shared.lua" );
+
+if SERVER then
+
+	resource.AddWorkshop( "185609021" )
+	include("atmos/init.lua");
+
+else
+
+	include("atmos/cl_init.lua");
+
+end
