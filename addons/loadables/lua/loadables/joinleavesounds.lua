e45b34296d73ccfc5f854e98d4d20cef6ee4b279
David Marcec <dmarcecguzman@gmail.com>
2018-06-30 11:14:31 +1000

    32 bits not needed, UNSIGNED & SPECTATOR team ids are 1001 & 1002

--- addons/loadables/lua/loadables/joinleavesounds.lua
+++ addons/loadables/lua/loadables/joinleavesounds.lua
@@ -3,14 +3,14 @@ if SERVER then
 	hook.Add( "PlayerInitialSpawn", "ZeagaPlayerJoin", function( ply )
 		net.Start( "ZeagaPlayerAttendance" ) -- We send the team/name and not the entity because it may not yet exist on the clients.
 			net.WriteBool( true )
-			net.WriteUInt( ply:Team( ), 32 )
+			net.WriteUInt( ply:Team( ), 10 )
 			net.WriteString( ply:Name( ) )
 		net.Broadcast( )
 	end )
 	hook.Add( "PlayerDisconnected", "ZeagaPlayerLeave", function( ply )
 		net.Start( "ZeagaPlayerAttendance" )
 			net.WriteBool( false )
-			net.WriteUInt( ply:Team( ), 32 )
+			net.WriteUInt( ply:Team( ), 10 )
 			net.WriteString( ply:Name( ) )
 		net.Broadcast( )
 	end )
@@ -32,7 +32,7 @@ if CLIENT then
 	}
 	net.Receive( "ZeagaPlayerAttendance", function( length )
 		local bArriving = net.ReadBool 	 ()
-		local iTeamId	= net.ReadUInt	 ( 32 )
+		local iTeamId	= net.ReadUInt	 ( 10 )
 		local strName	= net.ReadString ()
 
 		local iEventType = bArriving and TYPE_SPAWN or TYPE_DISCONNECT
