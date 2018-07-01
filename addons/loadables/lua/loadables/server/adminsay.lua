7b7d06fd14c2e3675b03ea5672f56a6059ebb629
C. Tyler Hart <zeaga15@gmail.com>
2018-06-30 20:19:34 -0700

    Added cvar for adminsay

--- addons/loadables/lua/loadables/server/adminsay.lua
+++ addons/loadables/lua/loadables/server/adminsay.lua
@@ -1,3 +1,5 @@
+CreateConVar( "rg_adminsay", "1", { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_LUA_SERVER }, "If enabled, team chat will relay to admin chat instead" )
+
 hook.Add( "PlayerSay", "AdminSay", function( ply, text, team )
 	if team then
 		ulx.asay( ply, text )
