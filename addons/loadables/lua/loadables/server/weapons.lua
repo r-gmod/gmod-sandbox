2e7614346ad20a6ee0af0e5534a43609932f0881
C. Tyler Hart <zeaga15@gmail.com>
2018-07-01 09:34:25 -0700

    Why does this happen to me

---
+++ addons/loadables/lua/loadables/server/weapons.lua
@@ -0,0 +1,8 @@
+CreateConVar( "rg_weapons", "1", { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_LUA_SERVER }, "If enabled, each player will receive a crowbar and gravity gun on each spawn" )
+
+hook.Add( "PlayerLoadout", "RGWeapons", function( ply )
+	if GetConVar( "rg_weapons" ):GetBool( ) then
+		ply:Give( "weapon_crowbar" )
+		ply:Give( "weapon_physcannon" )
+	end
+end )
